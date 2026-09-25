e334a057321e3de9dae0c28aecc46ab39368123d
Sol Sopheak <sopheaksol71@gmail.com>
2026-09-25 07:58:40 +0700

    chore: initialize SentinelFlow

---
+++ src/main/java/com/sopheak/SentinelFlow/SentinelFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sopheak.SentinelFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopheak/SentinelFlow/SentinelFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopheak.SentinelFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
